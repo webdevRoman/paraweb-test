ce9552d745af16c70d4e1c714d5e6e20d911def6
Roman <rgrabelnikov99@yandex.ru>
2020-03-03 18:44:17 +0700

    Initial commit, prepared project

---
+++ src/pages/about/about.js
@@ -0,0 +1,10 @@
+import './about.sass';
+import '../common/base.sass';
+
+// const sidebarBtn = document.querySelector('.blog-btn');
+// const sidebar = document.querySelector('.blog-sidebar');
+
+// sidebarBtn.addEventListener('click', () => {
+//   sidebar.classList.toggle('blog-sidebar_active');
+//   sidebarBtn.classList.toggle('blog-btn_active');
+// });
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const pug = require('./webpack/pug');
+const devserver = require('./webpack/devserver');
+const sass = require('./webpack/sass');
+const css = require('./webpack/css');
+const extractCSS = require('./webpack/css.extract');
+const vendors = require('./webpack/vendors');
+const images = require('./webpack/images');
+const fonts = require('./webpack/fonts');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const babel = require('./webpack/babel');
+
+const PATHS = {
+  src: path.join(__dirname, 'src'),
+  build: path.join(__dirname, 'dist')
+};
+
+const common = merge([
+  {
+    entry: {
+      'index': PATHS.src + '/pages/index/index.js',
+      'about': PATHS.src + '/pages/about/about.js'
+    },
+    output: {
+      path: PATHS.build,
+      filename: 'js/[name].js'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        chunks: ['index'],
+        template: PATHS.src + '/pages/index/index.pug'
+      }),
+      new HtmlWebpackPlugin({
+        filename: 'about.html',
+        chunks: ['about'],
+        template: PATHS.src + '/pages/about/about.pug'
+      }),
+      new CopyWebpackPlugin([
+        { from: `${PATHS.src}/img`, to: `${PATHS.build}/img` }
+      ])
+    ]
+  },
+  pug(),
+  images(),
+  fonts(),
+  babel()
+]);
+
+module.exports = function(env) {
+  if (env === 'production') {
+    return merge([
+      common,
+      extractCSS(),
+      vendors()
+    ]);
+  }
+  if (env === 'development') {
+    return merge([
+      common,
+      devserver(),
+      sass(),
+      css()
+    ]);
+  }
+};
