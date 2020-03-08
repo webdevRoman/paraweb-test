fc8e042497635bc8a007d1d89d243dfc6e5aff29
Roman <rgrabelnikov99@yandex.ru>
2020-03-08 19:02:34 +0700

    Made about page

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,10 +1,105 @@
-import './about.sass';
-import '../common/base.sass';
+import './about.sass'
+import '../common/base.sass'
 
-// const sidebarBtn = document.querySelector('.blog-btn');
-// const sidebar = document.querySelector('.blog-sidebar');
+class Slider {
+  constructor(container, items, itemWidth, gapWidth, btnLeft, btnRight) {
+    this.container = container
+    this.items = items
+    this.itemWidth = itemWidth
+    this.gapWidth = gapWidth
+    this.btnLeft = btnLeft
+    this.btnRight = btnRight
+    this.currentItem = 0
 
-// sidebarBtn.addEventListener('click', () => {
-//   sidebar.classList.toggle('blog-sidebar_active');
-//   sidebarBtn.classList.toggle('blog-btn_active');
-// });
+    this.container.style.marginLeft = '0px'
+    this.btnLeft.disabled = true
+    this.btnLeft.addEventListener('click', (e) => {
+      e.preventDefault()
+      this.slideRight()
+    })
+    this.btnRight.addEventListener('click', (e) => {
+      e.preventDefault()
+      e.preventDefault()
+      this.slideLeft()
+    })
+  }
+
+  slideRight() {
+    this.container.style.marginLeft = parseInt(this.container.style.marginLeft) + this.itemWidth + this.gapWidth + 'px'
+    this.currentItem--
+    if (this.currentItem == 0) {
+      this.btnLeft.disabled = true
+    } else {
+      this.btnLeft.disabled = false
+    }
+    if (this.currentItem == this.items.length - 2) {
+      this.btnRight.disabled = true
+    } else {
+      this.btnRight.disabled = false
+    }
+  }
+  slideLeft() {  
+    this.container.style.marginLeft = parseInt(this.container.style.marginLeft) - this.itemWidth - this.gapWidth + 'px'
+    this.currentItem++
+    if (this.currentItem == 0) {
+      this.btnLeft.disabled = true
+    } else {
+      this.btnLeft.disabled = false
+    }
+    if (this.currentItem == this.items.length - 2) {
+      this.btnRight.disabled = true
+    } else {
+      this.btnRight.disabled = false
+    }
+  }
+}
+
+const slider = new Slider(document.querySelector('.wellness-slider__items'), document.querySelectorAll('.wellness-slider__item'), 600, 30, document.querySelector('.wellness-slider__arrow_left'), document.querySelector('.wellness-slider__arrow_right'))
+
+
+class OrderValidator {
+  constructor(inputsClass, btnClass, errorClass) {
+    this.orderInputContainers = document.querySelectorAll(inputsClass)
+    this.orderInputs = []
+    this.orderInputContainers.forEach(container => {
+      for (let i = 0; i < container.childNodes.length; i++) {
+        if (container.childNodes[i].tagName == 'INPUT')
+        this.orderInputs.push(container.childNodes[i])
+      }
+    })
+    this.submitBtn = document.querySelector(btnClass)
+    this.errorClass = errorClass
+    this.orderFunc()
+  }
+  orderFunc() {
+    this.submitBtn.addEventListener('click', (e) => {
+      e.preventDefault()
+      if (this.isValid()) {
+        alert('Заявка отправлена.')
+        this.clearInputs()
+      } else {
+        for (let i = 0; i < this.orderInputContainers.length; i++) {
+          if (this.orderInputs[i].value =='')
+            this.orderInputContainers[i].classList.add(this.errorClass)
+        }
+      }
+    })
+    for (let i = 0; i < this.orderInputContainers.length; i++) {
+      this.orderInputs[i].addEventListener('input', () => {
+        this.orderInputContainers[i].classList.remove(this.errorClass)
+      })
+    }
+  }
+  isValid() {
+    for (let i = 0; i < this.orderInputs.length; i++)
+      if (this.orderInputs[i].value == '')
+        return false
+    return true
+  }
+  clearInputs() {
+    this.orderInputs.forEach(input => {
+      input.value = ''
+    })
+  }
+}
+const orderValidator = new OrderValidator('.order-input', '.order-btn', 'order-input_error')
